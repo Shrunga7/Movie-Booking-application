d04c11dc1eb06fa96b494e059fcff94d8d31cda6
Shrunga7 <shrunga.k.g767@gmail.com>
2022-07-23 17:32:53 +0530

    Header commit

---
+++ src/common/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Header.css';
+
+class Header extends React.Component{
+    render () {
+        return <div className="header"><img src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"/></div>;
+    }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
